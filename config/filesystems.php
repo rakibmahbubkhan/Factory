f76f66cf781c021510ae94fb9b1a1cef137aef59
rakibmahbubkhan <rak.mbk@gmail.com>
2026-03-10 16:18:51 +0600

    filesystems update

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),  // Changed from storage_path()
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
